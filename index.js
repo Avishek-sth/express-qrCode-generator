750e0928e633118a6747c38ed0a52b2db87cd5a1
Avishek Shrestha <avishek.shrestha16@gmail.com>
2023-08-30 15:00:34 +0545

    added public and view folders

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express();
 
 const indexRouter = require("./routes");
 
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.static("public"));
 app.use(express.json());
 app.use("/", indexRouter);
 
-
 app.listen(3333, () => {
-    console.log("Application is running on port 3333.");
+  console.log("Application is running on port 3333.");
 });
