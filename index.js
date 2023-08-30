96b5a73ba193b9107072fa640e89aab81610bb6e
Avishek Shrestha <avishek.shrestha16@gmail.com>
2023-08-30 21:25:10 +0545

    added error handling in route and app

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use("/", indexRouter);
 
+app.use((err, req, res, next) => {
+  err = err ? err.toString() : "Something went wrong!"
+  res.status(500).json({data: "", msg: err})
+})
+
 app.listen(3333, () => {
   console.log("Application is running on port 3333.");
 });
